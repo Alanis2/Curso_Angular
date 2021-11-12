96577619ca2d8eb633a5095a644e18defc09a00e
Alanis2 <alanisemanuela950@gmail.com>
2021-11-11 23:25:31 -0300

    Aula 14 Até 20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,9 +20,20 @@ export class AppComponent {
         Validators.required
       ])]
     });
-    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
-    this.todos.push(new Todo(2, 'Ir ao mercado', false));
-    this.todos.push(new Todo(3, 'Cortar O cabelo', false));
+
+    this.load();
+  }
+
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear(){
+    this.form.reset();
   }
 
   remove(todo: Todo){
@@ -30,14 +41,31 @@ export class AppComponent {
     if(index !== -1){
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo){
     todo.done = true;
-
+    this.save();
   }
 
   markAsUndone(todo: Todo){
     todo.done = false;
+    this.save();
+  }
+
+  save(){
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load(){
+    const data = localStorage.getItem('todos');
+    if(data){
+      this.todos = JSON.parse(data);
+    } else {
+      this.todos = [];
+    }
+    this.todos = JSON.parse(data);
   }
 }
